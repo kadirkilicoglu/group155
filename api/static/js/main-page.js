e588a7151a78068b9ea5466f5cf31060f75c17e3
bulbulelif <elifbulbulofficial@gmail.com>
2025-08-03 22:50:19 +0300

    patient-list-page eklendi

--- api/static/js/main-page.js
+++ api/static/js/main-page.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (roleId === 1) {
         window.location.href = "/authentication/admin-page"; // Admin sayfası
       } else if (roleId === 3) {
-        window.location.href = "/doctor-page"; // Doktor sayfası
+        window.location.href = "/patient-list-page"; // Doktor sayfası
       } else if (roleId === 2) {
         window.location.href = "/patient-page"; // Personel sayfası (örneğin hemşireler)
       } else {
